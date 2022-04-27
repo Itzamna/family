686084895a087ae7a2c382b92789dc26b424adf7
Elena Alesenko <omegka@gmail.com>
2022-04-27 14:21:45 +0300

    update info

--- bin/recreate-tree.js
+++ bin/recreate-tree.js
@@ -6,5 +6,5 @@ const lodash = require('lodash');
 lodash.keys(members).forEach((menu) => {
   fs.writeFileSync(
     `${__dirname}/../trees/pv-${menu}.json`,
-    `---\npid: ${menu}\n---\n{% include output-after.json %}`);
+    `---\npid: ${menu}\n---\n{% assign date = site.data.family%}{% assign piddata = site.data.family[page.pid] %}{% assign pidgender = piddata.gender %}[{{% capture fid %}{{ page.pid }}{% endcapture %}{% include people-json.html id=fid %},"textClass": "emphasis"{% if pidgender and pidgender != "#N/A" and pidgender != "FALSE" and pidgender != "0"  %},{% include plv-all.json fid=fid %}{% endif %}}]`);
 });
