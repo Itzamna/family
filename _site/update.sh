56ef596be258518f6cf2c008ec3a8aefa192ef3b
Elena Alesenko <omegka@gmail.com>
2025-12-04 16:37:57 +0300

    update info

--- _site/update.sh
+++ _site/update.sh
@@ -9,10 +9,12 @@ mkdir pages
 rm -rf trees || true
 mkdir trees
 ./bin/download.sh
+sleep 1m
 ./bin/recreate-pages.js
+sleep 2m
 ./bin/recreate-tree.js
-
+sleep 2m
 git pull
-git add . -A
+git add . 
 git commit -m'update info'
 git push origin gh-pages
